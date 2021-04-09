970205975fc1bc4643a69fc38113c38720a9096d
Priyanka Goenka <48933503+PriyankaGoenka@users.noreply.github.com>
2021-04-09 14:25:43 -0400

    Second_Project_Third_Task

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SecondProject.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'P2Task3.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
